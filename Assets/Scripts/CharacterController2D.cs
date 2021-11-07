7c3866dcca93312a8bcf75454e809207e098fb8e
Remy Streichenberger <remyastreich@gmail.com>
2021-11-07 10:59:54 -0500

    Player Anim

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
+    //animator components
+    public Animator anim;
     // Movement keys
     public bool hasDKey;
     public bool hasAKey;
@@ -50,7 +52,11 @@ public class CharacterController2D : MonoBehaviour
         if (hit.collider != null)
         {
             float distance = Mathf.Abs(hit.point.y - transform.position.y);
-            return (distance < .5f);
+            if (distance < .5f)
+            {
+                anim.SetBool("isJumping", false);
+                return true;
+            }            
         }
         return false;
     }
@@ -61,6 +67,7 @@ public class CharacterController2D : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         startSize = GetComponent<BoxCollider2D>().size;
         startOffset = GetComponent<BoxCollider2D>().offset;
+        anim = GetComponentInChildren<Animator>();
 
         blueBoxes = GameObject.FindGameObjectsWithTag("Blue");
         greenBoxes = GameObject.FindGameObjectsWithTag("Green");
@@ -76,24 +83,28 @@ public class CharacterController2D : MonoBehaviour
     {
         if (Input.GetKey("a") && hasAKey) {
             rigidBody.velocity = new Vector2(-speed, rigidBody.velocity.y);
-            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponentInChildren<SpriteRenderer>().flipX = false;
+            anim.SetBool("isWalking", true);
         } else if (Input.GetKey("d") && hasDKey) {
             rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
-            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponentInChildren<SpriteRenderer>().flipX = true;
+            anim.SetBool("isWalking", true);
         } else {
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            anim.SetBool("isWalking", false);
         }
         
         if (Input.GetKey("w") && hasWKey && IsGrounded()) {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
             GetComponent<AudioSource>().Play();
+            anim.SetBool("isJumping", true);
         }
         if (Input.GetKey("s") && hasSKey) {
-            GetComponent<SpriteRenderer>().size = new Vector2(1.0f, 0.5f);
+            GetComponentInChildren<SpriteRenderer>().size = new Vector2(1.0f, 0.5f);
             GetComponent<BoxCollider2D>().size = startSize * new Vector2(1.0f, 0.5f);
             GetComponent<BoxCollider2D>().offset = startOffset - new Vector2(0.0f, startSize.y * 0.25f);
         } else {
-            GetComponent<SpriteRenderer>().size = new Vector2(1.0f, 1.0f);
+            GetComponentInChildren<SpriteRenderer>().size = new Vector2(1.0f, 1.0f);
             GetComponent<BoxCollider2D>().size = startSize;
             GetComponent<BoxCollider2D>().offset = startOffset;
         }
